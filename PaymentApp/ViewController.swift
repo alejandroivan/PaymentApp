0e748d828935c3c8c3a88fc76c9ddd5421253cb8
Alejandro Melo Domínguez <alejandroivan@icloud.com>
2019-01-04 19:12:13 -0300

    Blank workspace

---
+++ PaymentApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PaymentApp
+//
+//  Created by Alejandro Melo Domínguez on 1/4/19.
+//  Copyright © 2019 Alejandro Melo Domínguez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
