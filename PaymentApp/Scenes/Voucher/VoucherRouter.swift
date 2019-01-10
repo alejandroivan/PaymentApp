110d0593878f6c06d1e8771346361c0d8a334484
Alejandro Melo Domínguez <alejandroivan@icloud.com>
2019-01-10 17:04:21 -0300

    Correct routing of the Voucher scene.

--- PaymentApp/Scenes/Voucher/VoucherRouter.swift
+++ PaymentApp/Scenes/Voucher/VoucherRouter.swift
@@ -13,7 +13,7 @@
 import UIKit
 
 @objc protocol VoucherRoutingLogic {
-    //func routeToSomewhere(segue: UIStoryboardSegue?)
+    func dismissModalScene()
 }
 
 protocol VoucherDataPassing {
@@ -25,33 +25,7 @@ class VoucherRouter: NSObject, VoucherRoutingLogic, VoucherDataPassing {
     var dataStore: VoucherDataStore?
 
     // MARK: Routing
-
-    //func routeToSomewhere(segue: UIStoryboardSegue?)
-    //{
-    //  if let segue = segue {
-    //    let destinationVC = segue.destination as! SomewhereViewController
-    //    var destinationDS = destinationVC.router!.dataStore!
-    //    passDataToSomewhere(source: dataStore!, destination: &destinationDS)
-    //  } else {
-    //    let storyboard = UIStoryboard(name: "Main", bundle: nil)
-    //    let destinationVC = storyboard.instantiateViewController(withIdentifier: "SomewhereViewController") as! SomewhereViewController
-    //    var destinationDS = destinationVC.router!.dataStore!
-    //    passDataToSomewhere(source: dataStore!, destination: &destinationDS)
-    //    navigateToSomewhere(source: viewController!, destination: destinationVC)
-    //  }
-    //}
-
-    // MARK: Navigation
-
-    //func navigateToSomewhere(source: VoucherViewController, destination: SomewhereViewController)
-    //{
-    //  source.show(destination, sender: nil)
-    //}
-
-    // MARK: Passing data
-
-    //func passDataToSomewhere(source: VoucherDataStore, destination: inout SomewhereDataStore)
-    //{
-    //  destination.name = source.name
-    //}
+    func dismissModalScene() {
+        viewController?.dismiss(animated: true, completion: nil)
+    }
 }
--- PaymentApp/Scenes/Voucher/VoucherViewController.swift
+++ PaymentApp/Scenes/Voucher/VoucherViewController.swift
@@ -47,17 +47,6 @@ class VoucherViewController: UIViewController, VoucherDisplayLogic {
         router.dataStore = interactor
     }
 
-    // MARK: Routing
-
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        if let scene = segue.identifier {
-            let selector = NSSelectorFromString("routeTo\(scene)WithSegue:")
-            if let router = router, router.responds(to: selector) {
-                router.perform(selector, with: segue)
-            }
-        }
-    }
-
     // MARK: View lifecycle
 
     override func viewDidLoad() {
@@ -71,6 +60,6 @@ class VoucherViewController: UIViewController, VoucherDisplayLogic {
     }
 
     func dismissPopup() {
-        dismiss(animated: true, completion: nil)
+        router?.dismissModalScene()
     }
 }
